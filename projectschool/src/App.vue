fe8b8a7cffde0ce4a6ae891a73b33bc975de8ac6
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2021-05-21 09:01:57 -0300

    Inicio de projeto padrão

--- projectschool/src/App.vue
+++ projectschool/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome Filipe to Your Vue.js App"/>
+  <Alunos/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Alunos from './components/Alunos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Alunos
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ projectschool/src/components/Alunos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{nome}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  //o data sempre tem que ter um return
+  data(){
+    return {
+      nome: "Alunos"
+    }
+  },
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
