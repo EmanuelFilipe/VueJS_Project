09fe3dd2f48c93543db242d7afbb45632ff37c01
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2021-05-27 09:50:48 -0300

    incluso botão 'Voltar' no componente Titulo e lógica para ele não ser exibido

--- projectschool/src/components/Aluno/AlunoDetalhe.vue
+++ projectschool/src/components/Aluno/AlunoDetalhe.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
-        <Titulo :texto="`Aluno: ${aluno.nome}`" />
-        <button>Editar</button> ||
-        <button>Cancelar</button>
-        <table>
+        <!-- :texto e :btnVoltar são parametros declarados no componente Titulo.vue -->
+        <Titulo :texto="`Aluno: ${aluno.nome}`" :btnVoltar="false">
+          <button class="btn btnEditar" @click="editar()">Editar</button>  
+        </Titulo>
+ 
+        <table id="table">
             <tbody>
                 <tr>
                     <td class="colPequeno">Matrícula:</td>
@@ -12,28 +14,28 @@
                     </td>
                 </tr>
                 <tr>
-                    <td>Nome:</td>
+                    <td class="colPequeno">Nome:</td>
                     <td>
                         <label>{{aluno.nome}}</label>
                         <input v-model="aluno.nome" type="text" />
                     </td>
                 </tr>
                 <tr>
-                    <td>Sobrenome:</td>
+                    <td class="colPequeno">Sobrenome:</td>
                     <td>
                         <label>{{aluno.sobrenome}}</label>
                         <input v-model="aluno.sobrenome" type="text" />
                     </td>
                 </tr>
                 <tr>
-                    <td>Data Nascimento:</td>
+                    <td class="colPequeno">Data Nascimento:</td>
                     <td>
                         <label>{{aluno.dataNasc}}</label>
                         <input v-model="aluno.dataNasc" type="text" />
                     </td>
                 </tr>
                 <tr>
-                    <td>Professor:</td>
+                    <td class="colPequeno">Professor:</td>
                     <td>
                         <label>{{aluno.professor.nome}}</label>
                         <select v-model="aluno.professor">
@@ -75,11 +77,38 @@ import Titulo from '../_Shared/Titulo'
                        }); 
         },
         methods: {
-            
+            editar(){
+                alert('teste')
+            }
         },
     }
 </script>
 
 <style scoped>
+    .btnEditar{
+        float: right;
+        background-color: rgb(76,186,249);
+    }
+    .colPequeno{
+        width: 20%;
+        text-align: right;
+        background-color: rgb(125, 217, 245);
+        font-weight: bold;
+    }
 
+    input, select {
+        margin: 0;
+        padding: 5px 10px;
+        font-size: 0.9em;
+        font-family: Montserrat;
+        border: 1px solid silver;
+        border-radius: 5px;
+        background-color: rgb(245,245,245);
+        width: 95%;
+    }
+
+    select {
+        height: 38px;
+        width: 100%;
+    }
 </style>
--- projectschool/src/components/_Shared/Titulo.vue
+++ projectschool/src/components/_Shared/Titulo.vue
@@ -1,17 +1,44 @@
 <template>
     <div>
-        <h1>{{texto}}</h1>
+        <div class="divTitulo">
+            <h1>{{texto}}</h1>
+            <div class="voltar">
+                <!-- slot - permite incluir outros elementos dentro do espaço daquele componente -->
+                <slot></slot>
+                <button v-show="btnVoltar" class="btn btnVoltar" @click="voltar()">Voltar</button>
+            </div>
+
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         props: {
-            texto: String
+            texto: String,
+            btnVoltar: Boolean
+        },
+        methods: {
+            voltar(){
+                this.$router.back();
+            }
         }
     }
 </script>
 
 <style scoped>
+    .divTitulo {
+        display: flex;
+        width: 50%;
+    }
+    .voltar{
+        width: 50%;
+        position: absolute;
+        margin-top: 25px;
+    }
 
+    .btnVoltar{
+        float: right;
+        background-color: rgb(199,199,199);
+    }
 </style>
